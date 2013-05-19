ebd1deed5d5a6dbde22a6447ad0d02cbc07ccfd3
David Bliss <proegssilb@gmail.com>
2013-05-19 18:46:21 -0400

    Script tweaks for slower screens

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+function nth_ocurrence(str, needle, nth) {
+  for (i=0;i<str.length;i++) {
+    if (str.charAt(i) == needle) {
+        if (!--nth) {
+           return i;    
+        }
+    }
+  }
+  return false;
+}
+
 function createTyper(url, selector, delay) {
+    var multiplier = 20;
+    var maxlen = 100;
     req = new XMLHttpRequest();
     req.onload = function () {
         var lines = this.responseText.split("\n");
@@ -6,26 +19,28 @@ function createTyper(url, selector, delay) {
         var typer = function () {
             elem = $(selector)
             // Get the new lines in
-            for (var i = 0; i < 10; i++) {
+            var text = "";
+            for (var i = 0; i < multiplier; i++) {
                 if (lineNo >= lines.length) {
                     lineNo = 0;
                 }
                 var newline = lines[lineNo].replace("<", "&lt;").replace(">", "&gt;")
-                elem.append(newline + "\n");
+                text = text + newline + "\n"
                 lineNo = lineNo + 1;
             }
+            elem.append(text)
             
             // Don't overflow.
             linecount = ( elem.text().match(/\n/g) || [] ).length
-            if (linecount > 500) {
-                newStart = elem.text().indexOf("\n")+1;
+            if (linecount > maxlen) {
+                newStart = nth_ocurrence(elem.text(), "\n", multiplier)+1;
                 elem.text( elem.text().substr(newStart) );
             }
             
             // Scroll to the bottom
             $("html, body").scrollTop( $("html, body").scrollTop() + 9999 );
         }
-        setInterval(typer, delay*10);
+        setInterval(typer, delay*multiplier);
     }
     req.open("get", url, true);
     req.send();
